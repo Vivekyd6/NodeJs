f29ce40f37d6660502647f5103b24164c2d20466
Vivek Yadav <vivekyadav36837@gmail.com>
2022-05-25 14:33:54 +0530

    Npm module
     Use of Node Js NPM Module :-
       Superheroes Module
      and its methods such as .random()
     to generate random superheroes name

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-console.log("Hello World");
+// console.log("Hello World");
 
-const fs = require("fs");
+// const fs = require("fs");
 
-fs.copyFileSync("file1.txt","file2.txt");
-console.log("A new file 2 created and the text of file1 ois copied to file2.txt");
+// fs.copyFileSync("file1.txt","file2.txt");
+// console.log("A new file 2 created and the text of file1 ois copied to file2.txt");
+
+
+var superheroes = require("superheroes");
+
+var mysuperheroes = superheroes.random();
+
+console.log(mysuperheroes);
