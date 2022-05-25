033a9550e4394cb0bc32ec1a73f0a856c93ee865
Vivek Yadav <vivekyadav36837@gmail.com>
2022-05-25 14:20:34 +0530

    Node Js mehtods

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 console.log("Hello World");
+
+const fs = require("fs");
+
+fs.copyFileSync("file1.txt","file2.txt");
+console.log("A new file 2 created and the text of file1 ois copied to file2.txt");
