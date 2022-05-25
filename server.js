5e28d095347c4fdfc6e95fbd66400cfd7c2d5afd
Vivek Yadav <vivekyadav36837@gmail.com>
2022-05-25 14:58:32 +0530

    Update server.js
    Use of get methods of express Js

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const express = require("express");
 // need to define it 
 const app = express();
 
+
+app.get("/" , function(req,resp){
+    // console.log(req);
+    resp.send("HELL0");
+})
 //use a method of express Js to use it  and use the callback function to show in terminal whether server service is started or not.
 app.listen(2000,function(){
     console.log("Server is running on port 2000");
